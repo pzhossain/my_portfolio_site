2bed375846c52a395c6091fad5cd0a1ad69e988e
Parvez <pzhossain@gmail.com>
2025-03-15 23:27:52 +0600

    Linking Pages & initial changes are made

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Page Routes
+
+Route::get('/', [SiteController::class, 'homePage'])->name ('home');
+
+Route::get('/about', [SiteController::class, 'aboutPage'])->name ('about');
+
+Route::get('/contact', [SiteController::class, 'contactPage'])->name ('contact');
+
+Route::get('/portfolio', [SiteController::class, 'portfolioPage'])->name ('portfolio');
+
+Route::get('/services', [SiteController::class, 'servicesPage'])->name ('services');
